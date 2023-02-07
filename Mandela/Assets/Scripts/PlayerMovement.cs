1ae9459d5be5939c45a0748d42e7c7231fe8bc6a
Menko2904 <124470441+Menko2904@users.noreply.github.com>
2023-02-07 15:59:57 +0100

    h

--- Mandela/Assets/Scripts/PlayerMovement.cs
+++ Mandela/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController controller; 
+    public CharacterController controller;
 
-    public float walkSpeed = 12f;
-    public float runSpeed = 20f;
-    public float gravity = -9.81f;
+    public float speed = 12f;
+    public float gravity = -9.8f;
     public float jumpHeight = 3f;
+    public float movement; 
+    public float crouchingHeight; 
+    public float standingHeight;
+
+    public bool isCrouching;    
+    public bool isSprinting = false;
+    public float sprintingMultiplier = 30f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -18,32 +24,73 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
+
     
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        if(isGrounded &&velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * walkSpeed * Time.deltaTime);
+
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+        }
+        else
+        {
+            isCrouching = false;
+        }
+
+        if (isCrouching = true)
+        {
+            controller.height = crouchingHeight;
+        }
+        else
+        {
+            controller.height = standingHeight;
+        }
+    
+
+        Vector3 movement = new Vector3();
+
+        movement = x * transform.right + z * transform.forward;
+
+        if (isSprinting == true)
+        {
+            movement *= sprintingMultiplier;
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
+        controller.Move(movement * speed * Time.deltaTime);
         controller.Move(velocity * Time.deltaTime);
     }
 }
-
